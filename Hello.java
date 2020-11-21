1f2534d78eaadf9865b9113e678f10c82db97144
disharohra <disha.rohra.0510@gmail.com>
2020-11-21 16:57:13 +0530

    commit 5

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ class Hello{
 	public static void main(String args[]){
 		System.out.println("Hello World!");
 		System.out.println("Hiii!");
-		System.out.println("Chal ab byee!")
+		System.out.println("Chal ab byee!");
+		System.out.println(1/0);
 	}
 }
